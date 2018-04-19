b17ff6e3c424bcb35c37c2e6aa5c628c53c2e13c
Rostislav Vatolin <vatolinrp@mail.ru>
2018-04-19 14:58:03 +0300

    simple authentication added

--- jogger-rest/src/main/java/com/vatolinrp/jogger/rest/service/JoggerService.java
+++ jogger-rest/src/main/java/com/vatolinrp/jogger/rest/service/JoggerService.java
@@ -2,11 +2,16 @@ package com.vatolinrp.jogger.rest.service;
 
 import com.vatolinrp.jogger.model.User;
 import com.vatolinrp.jogger.storage.SimpleDataBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -15,6 +20,8 @@ import java.util.List;
 @Produces( MediaType.APPLICATION_JSON )
 @Path( "/" )
 public class JoggerService {
+
+  private static final Logger logger = LoggerFactory.getLogger( JoggerService.class );
 
   @Autowired
   private SimpleDataBase simpleDataBase;
@@ -31,5 +38,17 @@ public class JoggerService {
   public List<User> getAllUsers()
   {
     return simpleDataBase.getUsers();
+  }
+
+  @POST
+  @Path("/users/{login}/runs")
+  public String addRun( @PathParam("login") final String login,
+    @HeaderParam("password") final String password )
+  {
+    final User jogger = simpleDataBase.getUsers().stream()
+      .filter( user -> login.equals( user.getLogin() ) && password.equals( user.getPassword() ) )
+      .findFirst().get();
+    logger.info( "User found!" );
+    return "";
   }
 }
